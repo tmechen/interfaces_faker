8b810ec2ffb03deb617c45a318718bdce120baef
thomas.mechenbier <mail@tmechen.me>
2020-06-02 12:27:12 +0200

    use socket

--- app.py
+++ app.py
@@ -10,27 +10,32 @@ with open(r'config.yaml') as file:
 
 for index, name in socket.if_nameindex():
     if name == config["interface"]:
-        print(f"config for {name} before:")
-        print(f"----------------------------------------------------")
-        print(subprocess.call((shlex.split(f"ip addr show {name}"))))
-        print(f"----------------------------------------------------")
+        print("config for {name} before:".format(name=name))
+        print("----------------------------------------------------")
+        print(subprocess.call((shlex.split("ip addr show {name}".format(name=name)))))
+        print("----------------------------------------------------")
 
         new_count = random.randint(1, config["max_ips"])
-        print(f"adding {new_count} new ip adresses for {config['valid_lft']} seconds")
+        print("adding {new_count} new ip adresses for {valid_lft} seconds".format(new_count=new_count,
+                                                                                  valid_lft=config['valid_lft']))
         random_ips = sorted(random.sample(
             population=config['ip_range']['hosts'],
             k=new_count))
         for random_ip in random_ips:
-            new_address = f"{config['ip_range']['network']}." \
-                          f"{config['ip_range']['subnet']}." \
-                          f"{random_ip}/" \
-                          f"{config['ip_range']['CIDR']}"
+            new_address = "{network}.{subnet}.{random_ip}/{CIDR}".format(
+                network=config['ip_range']['network'],
+                subnet=config['ip_range']['subnet'],
+                random_ip=random_ip,
+                CIDR=config['ip_range']['CIDR']
+            )
             subprocess.call(
-                shlex.split(f"ip addr add {new_address} dev "
-                            f"{name} valid_lft {config['valid_lft']} "
-                            f"preferred_lft 0"))
 
-        print(f"----------------------------------------------------")
-        print(f"config for {name} after:")
-        print(subprocess.call((shlex.split(f"ip addr show {name}"))))
-        print(f"----------------------------------------------------")
+                shlex.split("ip addr add {new_address} dev {name} valid_lft {valid_lft} preferred_lft 0".format(
+                    new_address=new_address,
+                    name=name,
+                    valid_lft=config['valid_lft'])))
+
+        print("----------------------------------------------------")
+        print("config for {name} after:".format(name=name))
+        print(subprocess.call((shlex.split("ip addr show {name}".format(name=name)))))
+        print("----------------------------------------------------")
