0fc870ad9e18f4029efd5aa8ab9af41ed6b826b1
thomas.mechenbier <mail@tmechen.me>
2020-06-02 10:40:49 +0200

    tr

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
 import ifcfg
+import yaml
+from pyroute2 import IPDB
+
+with open(r'config.yaml') as file:
+    config = yaml.load(file, Loader=yaml.FullLoader)
+    print(config["interface"])
 
 print(ifcfg.interfaces())
+
+for name, interface in ifcfg.interfaces().items():
+    if name == config["interface"]:
+        print(interface)
+
+    with IPDB() as ip:
+        ip.interfaces[name]. \
+            add_ip('172.16.0.1/24'). \
+            add_ip('172.16.0.2/24'). \
+            option('mtu', 1400). \
+            up(). \
+            commit()
