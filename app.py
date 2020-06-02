79a9a452566d6aecb6c5ba11859b2b07cbb3e2c7
thomas.mechenbier <mail@tmechen.me>
2020-06-02 11:11:42 +0200

    dxfg

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import random
 import shlex
 import subprocess
 
@@ -8,6 +9,10 @@ with open(r'config.yaml') as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
 
 for name, interface in ifcfg.interfaces().items():
-    print(name)
     if name == config["interface"]:
+        print(f"config for {name} before:")
+        print(f"----------------------------------------------------")
         print(subprocess.call((shlex.split(f"ip addr show {name}"))))
+        print(f"----------------------------------------------------")
+        for i in range(1, random.randint(1, config["max_ips"])):
+            print(i)
