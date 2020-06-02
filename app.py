a94914b5de3a41b6bee81f580245c48c286e35f0
thomas.mechenbier <mail@tmechen.me>
2020-06-02 11:06:41 +0200

    dxfg

--- app.py
+++ app.py
@@ -1,21 +1,12 @@
+import subprocess
+
 import ifcfg
 import yaml
-from pyroute2 import IPDB
 
 with open(r'config.yaml') as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
-    print(config["interface"])
-
-print(ifcfg.interfaces())
 
 for name, interface in ifcfg.interfaces().items():
+    print(name)
     if name == config["interface"]:
-        print(interface)
-
-    with IPDB() as ip:
-        ip.interfaces[name]. \
-            add_ip('172.16.0.1/24'). \
-            add_ip('172.16.0.2/24'). \
-            option('mtu', 1400). \
-            up(). \
-            commit()
+        print(subprocess.call([f"ip addr show {name}"]))
